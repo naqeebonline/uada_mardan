6371e30f4848b025c640bf18de22184003f119c9
Naqeeb <naqeebonline@gmail.com>
2025-09-18 12:09:51 +0500

    chnages done

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -14,6 +14,7 @@ class AdminDashboardController extends Controller
 {
     public function getDashboardData()
     {
+        
 
         $data["title"] = "User Registration";
         $data["total_tma_properties"] = $this->getTotalTmaProperties();
