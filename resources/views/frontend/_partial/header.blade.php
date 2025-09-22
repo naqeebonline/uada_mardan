4f2f32b75974551935637ca33a73b6050ea9663b
Naqeeb <naqeebonline@gmail.com>
2025-09-22 21:59:23 +0500

    chnages

--- resources/views/frontend/_partial/header.blade.php
+++ resources/views/frontend/_partial/header.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="potenzaglobalsolutions.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{$title ?? ""}}</title>
+    <title>UADA Mardan</title>
 
     <!-- Favicon -->
 
